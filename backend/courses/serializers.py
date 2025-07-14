2b2c76cb027b558187d09a849e8ea434884e1e60
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2025-07-14 18:11:34 +0300

    update serializer course

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Course
 
 class CourseSerializer(serializers.ModelSerializer):
+    video_file = serializers.SerializerMethodField()
+    
     class Meta:
         model = Course
         fields = '__all__'
+        
+    def get_video_file(self, obj):
+        if obj.video:
+            return obj.video.url
+        return None
